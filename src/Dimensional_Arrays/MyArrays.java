4fc55b9a4ca08392169f35a0e70cc7a4df8d2086
NTT-CKN <tintaptucteo@gmail.com>
2026-07-16 01:46:54 +0700

    array_1

---
+++ src/Dimensional_Arrays/MyArrays.java
@@ -0,0 +1,141 @@
+package Dimensional_Arrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class MyArrays {
+    private int[] array;
+    public MyArrays(int[] array) {
+        this.array = array;
+    }
+    public int[] mirror(){
+        int[] result = new int[this.array.length * 2];
+        for (int i = 0; i < this.array.length; i++) {
+            result[i] = array[i];
+        }
+        for (int i = 0; i < this.array.length; i++) {
+            result[i + this.array.length] = array[array.length - 1 - i];
+        }
+        return result;
+    }
+    public int[] removeDuplicates(){
+        int[] result = new int[this.array.length];
+        int size = 0;
+        boolean flag;
+        for (int i = 0; i < this.array.length; i++) {
+            flag = false;
+            for (int j = 0; j < size - 1; j++) {
+                if (array[i]==result[j]) {
+                    flag = true;
+                }
+            }
+            if (!flag) {
+                result[size++] = array[i];
+            }
+        }
+        return Arrays.copyOf(result, size);
+    }
+    public boolean isSorted(){
+        for (int i = 0; i < this.array.length-1; i++) {
+            if (this.array[i] > this.array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public int[] getMissingValues(){
+        // tim min cua mang
+       // tim max cua mang
+        int count = 0;
+        for(int i = findMin(); i<=findMax(); i++){
+            if(!contains(i)){
+                count++;
+            }
+        }
+        int[] result = new int[count];
+        int index = 0;
+        for(int i = findMin(); i<=findMax(); i++){
+            if(!contains(i)){
+                result[index] =i;
+                index++;
+            }
+        }
+    return result;
+    }
+    public int findMin(){
+        int min = array[0];
+        for (int i = 0; i < this.array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+    public int findMax(){
+        int max = array[0];
+        for (int i = 0; i < this.array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+    public boolean contains(int value){
+        for (int i = 0; i < this.array.length; i++) {
+            if (array[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public int[] fillMissingValues(int k) {
+        int n = array.length;
+        int[] result = array.clone();
+
+        for (int i = 0; i < n; i++) {
+            boolean isMissing = array[i] == -1
+                    || (i > 0 && array[i] <= array[i - 1]);
+
+            if (isMissing) {
+                int sum = 0, count = 0;
+
+                // k phần tử liền trước (theo vị trí cố định)
+                for (int j = Math.max(0, i - k); j < i; j++) {
+                    if (array[j] != -1) {
+                        sum += array[j];
+                        count++;
+                    }
+                }
+                // k phần tử liền sau
+                for (int j = i + 1; j <= Math.min(n - 1, i + k); j++) {
+                    if (array[j] != -1) {
+                        sum += array[j];
+                        count++;
+                    }
+                }
+
+                if (count > 0) {
+                    result[i] = sum / count; // chia nguyên = floor (vì giá trị dương)
+                }
+            }
+        }
+        return result;
+    }
+    static void main() {
+        int[] array = {10, 11, 12, -1 ,14, 10, 17, 19 ,20};
+        System.out.println(array.length);
+        // mirror
+        MyArrays myArrays = new MyArrays(array);
+//        System.out.println(Arrays.toString(myArrays.mirror()));
+        // removeDuplicate
+//        System.out.println(Arrays.toString(myArrays.removeDuplicates()));
+        // isSorted
+        System.out.println(myArrays.isSorted());
+
+        System.out.println(Arrays.toString(myArrays.getMissingValues()));
+        System.out.println(Arrays.toString(myArrays.fillMissingValues(3)));
+
+
+    }
+}
